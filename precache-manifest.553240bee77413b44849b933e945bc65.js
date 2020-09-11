38f43bcc7ac122e36fe07c098279a2eec7d0ee2a
Konstantin Krumin <konstantin.krumin@gmail.com>
2020-09-11 20:16:38 +0300

    Updates

---
+++ precache-manifest.553240bee77413b44849b933e945bc65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1116ec7c411637bcf22b77218443fef6",
+    "url": "/dataviz-choropleth-map-d3/index.html"
+  },
+  {
+    "revision": "7ed998ceb6bf58004990",
+    "url": "/dataviz-choropleth-map-d3/static/css/main.456c74c5.chunk.css"
+  },
+  {
+    "revision": "d0dc8a94dbe988a7d379",
+    "url": "/dataviz-choropleth-map-d3/static/js/2.2f882ec1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dataviz-choropleth-map-d3/static/js/2.2f882ec1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ed998ceb6bf58004990",
+    "url": "/dataviz-choropleth-map-d3/static/js/main.a34c1e64.chunk.js"
+  },
+  {
+    "revision": "1c922384a435dfce99ad",
+    "url": "/dataviz-choropleth-map-d3/static/js/runtime-main.39126bc4.js"
+  }
+]);
